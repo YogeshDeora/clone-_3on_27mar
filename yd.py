13ee9b94e8ea896701e82321d4a4a7119b11b11d
Yogesh Deora <yogeshdeora428@gmail.com>
2024-03-27 15:29:25 +0530

    py file added

---
+++ yd.py
@@ -0,0 +1,11 @@
+order of statement matter here
+
+age=int(input("how old are u?"))
+if age==19:
+   print("yogesh")
+elif age >=18:
+    print("You are an adult!")
+elif age <0:
+    print("u are not born yet")
+else:
+    print ("you are a kid!")
